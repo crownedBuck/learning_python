3b7be3793664ff30956f2ac2d35084b95fc026f7
Laura Hart <onemansavedall@hotmail.com>
2024-02-24 01:56:08 -0600

    second .py file added

---
+++ introducing_lists.py
@@ -0,0 +1,50 @@
+# Lists
+
+bicycles = ['treck', 'cannondale', 'redline', 'specialized']
+
+print(bicycles)
+
+print(bicycles[0]) #like every other language I've learned so far. 
+
+print(bicycles[0].title())
+
+# Index Positions Start at 0 not one, like every other language I know
+
+print(bicycles[1])
+print(bicycles[3])
+
+print(bicycles[-1]) #ALWAYS return last item on the list. This syntax is new as most languages would return an error. At least from the ones I know
+
+message = f"My first bicycle was a {bicycles[0].title()}"
+
+print(message)
+
+print('')
+print('')
+print('')
+
+print('Exercise')
+
+name = ['Mark', 'Sarah', 'Seth', 'Pickles']
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
+car = ['Porche', 'Astin Martin', 'Impalla']
+
+message = "I would like to own a"
+
+mes = f"{message} {car[0]}"
+print(mes)
+
+mes =f"{message} {car[1]}"
+print(mes)
+
+mes = f"{message}n {car[2]}"
+print(mes)
+
+# Modifying, Adding, and Removing Elements
+
+# Modifying
+
